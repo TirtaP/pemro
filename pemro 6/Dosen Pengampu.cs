870c129c8aab89760765d79fc8bd8ccdcd871976
TirtaP <arif.kurniawan@students.amikom.ac.id>
2023-04-10 15:43:54 +0700

    commit 1

---
+++ pemro 6/Dosen Pengampu.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pemro_6
+{
+    class Dosen_Pengampu
+    {
+        private int ID;
+        private string Nama;
+        private int NIK;
+        private string Gender;
+        private string course;
+
+        {
+
+    }
+}
---
+++ pemro 6/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pemro_6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Dosen dosen = new Dosen();
+            Console.WriteLine("Default Dosen Pengampu:");
+            dosen.display();
+
+            Console.WriteLine("\nMasukkan Nama dan NIK Dosen Baru:");
+            dosen.attDosen(Console.ReadLine(), Convert.ToInt32(Console.ReadLine()));
+            dosen.display();
+            Console.Writeline("\nMasukkan Mata Kuliah Baru");
+            Dosen.addCourse(Console.ReadLine());
+            dosen.display();
+
+            Console.ReadLine();
+        }
+    }
+}
